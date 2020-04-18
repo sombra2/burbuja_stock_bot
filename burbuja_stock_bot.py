187b3df03980a1abc1ece0253289ba09ea480b1b
sombra2 <sombra2@gmail.com>
2020-04-18 20:58:12 +0400

    telegram library update
    No funciona la librería del wrapper del API

--- burbuja_stock_bot.py
+++ burbuja_stock_bot.py
@@ -5,6 +5,7 @@ de los users y que devuelva las cotizaciones de Yahoo Finance a tiempo real
 
 import yfinance as yf
 import requests
+from telegram.ext import Updater
 import os
 import urllib.parse #librería necesaria para codificar el mensaje a enviar
 
@@ -13,12 +14,6 @@ version = '1.0'
 
 
 # CUERPO DEL SCRIPT
-
-
-
-
-
-
 
 
 '''Esta función es la encargada de mandar el mensaje al canal. Tanto el token del bot como el chatID corresponden
@@ -33,6 +28,4 @@ def telegram_bot_sendtext(bot_message):
   send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=markdown&text=' + bot_message
   response = requests.get(send_text)
   return response.json()
-
-telegram_bot_sendtext('prueba desde pycharm')
 
