3ec5863383982c61d5e1ff6ccce4ace0a6fce113
sombra2 <sombra2@gmail.com>
2020-04-18 20:41:40 +0400

    first version

--- burbuja_stock_bot.py
+++ burbuja_stock_bot.py
@@ -3,11 +3,13 @@ En este script vamos a intentar crear un bot para el canal de Telegram que esté
 de los users y que devuelva las cotizaciones de Yahoo Finance a tiempo real
 '''
 
-
 import yfinance as yf
 import requests
+import os
 import urllib.parse #librería necesaria para codificar el mensaje a enviar
 
+cwd = os.path.dirname(os.path.realpath(__file__))
+version = '1.0'
 
 
 # CUERPO DEL SCRIPT
@@ -22,10 +24,15 @@ import urllib.parse #librería necesaria para codificar el mensaje a enviar
 '''Esta función es la encargada de mandar el mensaje al canal. Tanto el token del bot como el chatID corresponden
 a un canal de pruebas. Solicita el acceso a @sombra2517 para hacer pruebas
 '''
+with open(cwd + '/token_info.txt', 'r') as f:
+  f = f.readlines()
+
 def telegram_bot_sendtext(bot_message):
-  bot_token = '1134886701:AAEUvVcyb77iTz_VuTraVkC75oH83O82ik4' # token para el bot de pruebas
-  bot_chatID = '-1001123997984' # chatID del canal de pruebas
+  bot_token = f[0].strip() # token para el bot de pruebas
+  bot_chatID = f[1].strip() # chatID del canal de pruebas
   send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=markdown&text=' + bot_message
   response = requests.get(send_text)
   return response.json()
+
+telegram_bot_sendtext('prueba desde pycharm')
 
